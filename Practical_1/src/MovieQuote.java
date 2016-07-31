2418d0bf49ced91a44e0166ab0ed2ff768eaf1f8
Jake Wood <jake.wood@my.jcu.edu.au>
2016-08-01 09:39:47 +1000

    removed gitignore

---
+++ Practical_1/src/MovieQuote.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Jake on 1/08/2016.
+ */
+public class MovieQuote {
+    public static void main(String[] args) {
+        System.out.println("\"Hey, don't knock masturbation. It's sex with someone I love.\" - ALVY SINGER (Woody Allen) in Annie Hall (1977)");
+    }
+}
